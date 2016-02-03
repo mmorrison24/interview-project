9098131d674176a7f380d625ae5da60fdfcac412
mmorrison24 <M24@MMs-MacBook-Pro.local>
2016-02-02 19:39:19 -0500

    added functionality to fetch tweets from twitter api

--- twitter-viewer.js
+++ twitter-viewer.js
@@ -1,16 +1,30 @@
 "use strict";
+//node_modules
 let express = require('express');
+let Twitter = require('Twitter');
 
+//application modules
 let config = require('./lib/config');
+
 
 const port = 8081;
 
-let app = express();
-config.applyConfiguration(app);
-config.applyRouting(app);
+//application bootstrap
+let twitter_client = new Twitter({
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
 
+let app = express();
+config.applyConfiguration( app );
+config.applyRouting( app, twitter_client );
+
+
+//start express server
 let server = app.listen(port, function () {
 
-    console.log(`listening on port: ${server.address().port}`);
+    console.log( `listening on port: ${server.address().port}` );
 
 });
